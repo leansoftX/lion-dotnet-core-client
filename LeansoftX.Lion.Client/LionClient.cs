84c1765c70a92f7e5fbb3fd1fad567fc05be967a
振龙 魏 <VincentWei@leansoftx.com>
2019-01-15 21:08:46 +0800

    [Lion SDK] url && api request style

--- LeansoftX.Lion.Client/LionClient.cs
+++ LeansoftX.Lion.Client/LionClient.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace LeansoftX.Lion.Client
 {
@@ -9,64 +8,16 @@ namespace LeansoftX.Lion.Client
     {
         private string _sdkKey { get; set; }
 
-        private const string LionAPIURL = "http://lion-test.devcloudx.com/";
+        private const string LionAPIURL = "http://lion-test.devcloudx.com/api/";
 
         public LionClient(string sdkKey)
         {
             _sdkKey = sdkKey;
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
         }
 
         public bool BoolVariation(string key)
         {
-            var url = LionAPIURL + "/api/FlagStatuses/" + key + "/" + _sdkKey;
-            var result = Get(url);
-            return Boolean.Parse(result);
+            return bool.Parse(new HttpClient().GetStringAsync(string.Format("{0}FlagStatuses/{1}/{2}", LionAPIURL, key, _sdkKey)).Result);
         }
-
-
-        #region Api
-        private string Get(string sURL)
-        {
-            using (HttpWebResponse webResponse = GetResponse(sURL))
-            using (Stream stream = webResponse.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        private string Post(string postUrl, string paramData)
-        {
-            return PostWebRequest(postUrl, paramData, new UTF8Encoding());
-        }
-
-
-        private string PostWebRequest(string postUrl, string paramData, Encoding dataEncode)
-        {
-            byte[] byteArray = dataEncode.GetBytes(paramData);
-            HttpWebRequest webReq = (HttpWebRequest)WebRequest.Create(new Uri(postUrl));
-            webReq.Method = "POST";
-            webReq.ContentType = "application/x-www-form-urlencoded";
-
-            webReq.ContentLength = byteArray.Length;
-            using (Stream newStream = webReq.GetRequestStream())
-            {
-                newStream.Write(byteArray, 0, byteArray.Length);
-            }
-            using (HttpWebResponse response = (HttpWebResponse)webReq.GetResponse())
-            using (StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.Default))
-            {
-                return sr.ReadToEnd();
-            }
-        }
-
-        private HttpWebResponse GetResponse(string sUrl)
-        {
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(sUrl);
-            return (HttpWebResponse)webRequest.GetResponse();
-        }
-        #endregion
-
     }
 }
