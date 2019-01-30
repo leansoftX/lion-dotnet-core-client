a579f560baf84684b2b473bc1e550f8041208833
jackyzhou <jackyzhou@lean-soft.cn>
2019-01-30 11:12:36 +0800

    支持客户端自定义属性设置

--- Lion.Client.SDK/LionUser.cs
+++ Lion.Client.SDK/LionUser.cs
@@ -17,6 +17,9 @@ namespace Lion.Client.SDK
         [JsonProperty(PropertyName = "email", NullValueHandling = NullValueHandling.Ignore)]
         public string Email { get; set; }
 
+        [JsonProperty(PropertyName = "custom", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, JToken> Custom { get; set; }
+
         public LionUser(string key)
         {
             Key = key;
@@ -25,6 +28,72 @@ namespace Lion.Client.SDK
         public static LionUser WithKey(string key)
         {
             return new LionUser(key);
+        }
+
+
+    }
+    public static class UserExtensions
+    {
+        public static LionUser AndName(this LionUser user, string name)
+        {
+            user.Name = name;
+            return user;
+        }
+
+        public static LionUser AndEmail(this LionUser user, string email)
+        {
+            user.Email = email;
+            return user;
+        }
+
+        public static LionUser AndCustomAttribute(this LionUser user, string attribute, string value)
+        {
+            if (attribute == string.Empty)
+                throw new ArgumentException("Attribute Name can not be empty");
+
+            user.Custom.Add(attribute, new JValue(value));
+
+            return user;
+        }
+
+        public static LionUser AndCustomAttribute(this LionUser user, string attribute, bool value)
+        {
+            if (attribute == string.Empty)
+                throw new ArgumentException("Attribute Name can not be empty");
+
+            user.Custom.Add(attribute, new JValue(value));
+
+            return user;
+        }
+
+        public static LionUser AndCustomAttribute(this LionUser user, string attribute, int value)
+        {
+            if (attribute == string.Empty)
+                throw new ArgumentException("Attribute Name can not be empty");
+
+            user.Custom.Add(attribute, new JValue(value));
+
+            return user;
+        }
+
+        public static LionUser AndCustomAttribute(this LionUser user, string attribute, float value)
+        {
+            if (attribute == string.Empty)
+                throw new ArgumentException("Attribute Name can not be empty");
+
+            user.Custom.Add(attribute, new JValue(value));
+
+            return user;
+        }
+
+        public static LionUser AndCustomAttribute(this LionUser user, string attribute, List<string> value)
+        {
+            if (attribute == string.Empty)
+                throw new ArgumentException("Attribute Name can not be empty");
+
+            user.Custom.Add(attribute, new JArray(value.ToArray()));
+
+            return user;
         }
 
     }
